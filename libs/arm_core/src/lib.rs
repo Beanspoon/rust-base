00d308065bbde4f9c59b3242feaa85c8c983e72c
Peter Kubiak <peter.s.kubiak@bath.edu>
2023-11-26 03:05:31 +0000

    Add main function

---
+++ libs/arm_core/src/lib.rs
@@ -0,0 +1,21 @@
+#![no_std]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub unsafe extern "C" fn Reset() -> ! {
+    extern "Rust" {
+        fn main() -> !;
+    }
+
+    main()
+}
+
+#[link_section = ".vector_table.reset_vector"]
+#[no_mangle]
+pub static RESET_VECTOR: unsafe extern "C" fn() -> ! = Reset;
+
+#[panic_handler]
+fn panic(_panic: &PanicInfo<'_>) -> ! {
+    loop {}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,11 @@
-#![no_main]
 #![no_std]
+#![no_main]
 
-use core::panic::PanicInfo;
-
-#[panic_handler]
-fn panic(_panic: &PanicInfo<'_>) -> ! {
-    loop {}
-}
+extern crate arm_core;
 
 #[no_mangle]
-pub unsafe extern "C" fn Reset() -> ! {
+pub fn main() -> ! {
+    let _x = 42;
+
     loop {}
 }
-
-#[link_section = ".vector_table.reset_vector"]
-#[no_mangle]
-pub static RESET_VECTOR: unsafe extern "C" fn() -> ! = Reset;
